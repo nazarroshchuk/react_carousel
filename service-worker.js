44f119934f8617bda52be56d4f71668f6eeeb5d1
Nazarii <nazarrishchuk@gmail.com>
2021-04-22 17:58:53 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c3eae33d7ac1f65680fe9ea5b458330e.js"
+  "./precache-manifest.8b44e415c10a16f34ec4791edcc1c516.js"
 );
 
 self.addEventListener('message', (event) => {
